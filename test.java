8e2b695e815894c25acc4cc6fced022d663050b5
rolim91 <rolim91@shaw.ca>
2015-03-22 13:33:37 -0700

    Initial commit with class Talisman and test file

---
+++ test.java
@@ -0,0 +1,14 @@
+
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		Talisman hello = new Talisman();
+		System.out.println(hello);
+	}
+
+}
